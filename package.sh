6f684b82e31183488b8cbe310a8954430f268c89
xuxihai <xuxihai123@gmail.com>
2024-02-23 22:02:21 +0800

    update package.sh

--- package.sh
+++ package.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-GWK_VERSION=`git describe --tags --abbrev=0`
+output_dir="./release"
+
+APP_VERSION=`git describe --tags --abbrev=0`
 GIT_HASH=`git rev-parse --short HEAD`
-echo $GWK_VERSION'-'$GIT_HASH
+echo $APP_VERSION'-'$GIT_HASH
 
 cwd=`pwd`
 
 
-rm -rf ./release
-mkdir -p ./release
+rm -rf $output_dir
+mkdir -p $output_dir
 
-# os_all='linux windows darwin freebsd'
-# arch_all='386 amd64 arm arm64'
 
+# platforms=( "darwin/amd64" "darwin/arm64" )
 platforms=("windows/amd64" "windows/386" "windows/arm64"  "linux/amd64" "linux/386" "linux/arm64" "darwin/amd64" "darwin/arm64" "freebsd/amd64" "freebsd/386" "freebsd/arm64" )
 
 for platform in "${platforms[@]}"
@@ -21,31 +22,31 @@ do
     os=${platform_split[0]}
     arch=${platform_split[1]}
 
-    targetzipname="gwk_${GWK_VERSION}_${os}_${arch}"
-    gwk_outdir="release/${targetzipname}"
-    output_name1=gwk
-    output_name2=gwkd
-    mkdir -p $gwk_outdir
+    targetzip_name="gwk_${APP_VERSION}_${os}_${arch}"
+    build_outdir="${output_dir}/${targetzip_name}"
+    platform_bin1=gwk
+    platform_bin2=gwkd
+    mkdir -p $build_outdir
     if [ $os = "windows" ]; then
-        output_name1+='.exe'
-        output_name2+='.exe'
+        platform_bin1+='.exe'
+        platform_bin2+='.exe'
     fi
-    echo "Build release/${targetzipname}...";\
-    env CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} go build -trimpath -ldflags "-X main.Version=$GWK_VERSION -X main.GitCommitHash=$GIT_HASH" -o ${gwk_outdir}/${output_name1} ./bin/gwk
-    env CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} go build -trimpath -ldflags "-X main.Version=$GWK_VERSION -X main.GitCommitHash=$GIT_HASH"  -o ${gwk_outdir}/${output_name2} ./bin/gwkd
-    echo "Build ${gwk_outdir} done";
+    echo "Build ${build_outdir}...";\
+    env CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} go build -trimpath -ldflags "-X main.Version=$APP_VERSION -X main.GitCommitHash=$GIT_HASH" -o ${build_outdir}/${platform_bin1} ./bin/gwk
+    env CGO_ENABLED=0 GOOS=${os} GOARCH=${arch} go build -trimpath -ldflags "-X main.Version=$APP_VERSION -X main.GitCommitHash=$GIT_HASH"  -o ${build_outdir}/${platform_bin2} ./bin/gwkd
+    echo "Build ${build_outdir} done";
 
-    cp ./LICENSE ${gwk_outdir}
-    cp -rf ./etc ${gwk_outdir}
+    cp ./LICENSE ${build_outdir}
+    cp -rf ./etc ${build_outdir}
 
     # packages
-    cd release
+    cd $output_dir
     if [ $os = "windows" ]; then
-        zip -rq ${targetzipname}.zip ${targetzipname}
+        zip -rq ${targetzip_name}.zip ${targetzip_name}
     else
-        tar -zcf ${targetzipname}.tar.gz ${targetzipname}
+        tar -zcf ${targetzip_name}.tar.gz ${targetzip_name}
     fi  
-    rm -rf ${targetzipname}
+    # rm -rf ${targetzip_name}
     cd ..
 done
 
