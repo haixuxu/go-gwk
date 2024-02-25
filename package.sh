d9a078d3fa9f2deea4c41445b418b52a8daf0c37
xuxihai <xuxihai123@gmail.com>
2024-02-25 20:55:36 +0800

    chore(cli): update package.sh

--- package.sh
+++ package.sh
@@ -6,15 +6,13 @@ APP_VERSION=`git describe --tags --abbrev=0`
 GIT_HASH=`git rev-parse --short HEAD`
 echo $APP_VERSION'-'$GIT_HASH
 
-cwd=`pwd`
-
-
 rm -rf $output_dir
 mkdir -p $output_dir
 
-
 # platforms=( "darwin/amd64" "darwin/arm64" )
 platforms=("windows/amd64" "windows/386" "windows/arm64"  "linux/amd64" "linux/386" "linux/arm64" "darwin/amd64" "darwin/arm64" "freebsd/amd64" "freebsd/386" "freebsd/arm64" )
+# 写入_sha256_checksums.txt文件
+checksum_txt=gwk_sha256_checksums.txt
 
 for platform in "${platforms[@]}"
 do
@@ -43,10 +41,12 @@ do
     cd $output_dir
     if [ $os = "windows" ]; then
         zip -rq ${targetzip_name}.zip ${targetzip_name}
+        sha256sum "$targetzip_name.zip">> $checksum_txt
     else
         tar -zcf ${targetzip_name}.tar.gz ${targetzip_name}
+        sha256sum "$targetzip_name.tar.gz">> $checksum_txt
     fi  
-    # rm -rf ${targetzip_name}
+    rm -rf ${targetzip_name}
     cd ..
 done
 
